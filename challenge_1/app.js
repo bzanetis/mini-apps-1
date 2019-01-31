8821a5f0d7fb21a42ec92f3b1032773a093de20d
bzanetis <britt.zanetis19@gmail.com>
2019-01-31 11:22:27 -0600

    Implemented check for board state and call to check for each type of win

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,13 +3,18 @@ console.log("let's party")
 
 //array possibly? updates after each turn
 var startBoard = [["", "", ""],[ "", "", ""],["", "", ""]];
+var activeBoard = [["", "", ""],[ "", "", ""],["", "", ""]];
+var counter = 0;
+
+var resetData = function() {
+  startBoard = [];
+
+}
 
 //currentBoard ---> updates each play by .push into inner row array
 //check after each push to see if there's a winner
 //[row, col] coordinate (nested for loop)
 
-
-var counter = 0;
 
 
 //create a whoseTurn func that alternates between x and o boolean values
@@ -20,39 +25,73 @@ function whoseTurn() {
     return "X";
   } else {
     counter++;
-    return "0";
+    return "O";
   }
 };
 
-
-//truth check on if it's empty
-//when below is run --> need to set the [row, col] in board array
-//
+//func that will mark square with an X or O depending on where is clicked and which turn
 function markCellOne() {
 
   if (event.target.innerHTML === "") {
-    event.target.innerHTML = whoseTurn();
+    var player = whoseTurn();
+    event.target.innerHTML = player;
     var col = event.target.id;  //index of each row
     var row = event.target.className;  //each row is an inner array of col vals
-    console.log(row, col)
-    updateBoard(row, col);
-  };
-};
+    activeBoard[row][col] = player;
+    checkForWinner(activeBoard);
+    //console.log(activeBoard)
+    //startBoard[row][col] = event.target.innerHTML;
 
-// horiz --> check all within target row
-//vertical --> check all rows at index = col
-//func to update board
-function updateBoard(row, col) {
-  if (counter % 2 === 0) {
-    startBoard[row][col] = "X";
-  } else {
-    startBoard[row][col] = "O";
-  };
-  console.log(startBoard)
+  }
+
 };
 
 
+//check board ---> check for horizontal win [0, 0] + [0, 1] + [0, 2] etc..
+function checkForWinner (activeBoard) {
+  //console.log(activeBoard)
+  var col = event.target.id;
+  var row = event.target.className;
+  var horizPlays = [];
+  var vertPlays = [];
+  var diagonalPlays = [];
 
-//func to check board
+
+  var winCheckForHorizontal = function(activeBoard) {
+//console.log(activeBoard)
+    //loop through to get board coords
+    for (var i = 0; i < activeBoard.length; i++) {
+      for (var j = 0; j < activeBoard[i].length; j++) {
+
+        console.log(activeBoard)
+        // console.log(activeBoard[i])
+
+        //check if spot clicked [row, col] = a vert win, if so push X into vertPlays arr
+        if (activeBoard.allValuesSame()) {
+          return "x wins!";
+
+        };
+        // if (([row, col] = ["0", "0"]) || ([row, col] = ["0", "1"]) || ([row, col] = ["0", "2"])) {
+        //   return "x wins!"
+          // console.log(horizPlays)
+        // }
+      }
+    }
+
+  }
+
+  winCheckForHorizontal(activeBoard);
+
+};
+
+Array.prototype.allValuesSame = function() {
+  for(var i = 1; i < this.length; i++){
+    if(this[i] !== this[0])
+    return false;
+    }
+    return true;
+}
+
+
 
 
